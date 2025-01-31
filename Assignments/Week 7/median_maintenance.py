e6562f2a9c34a7b7f24bc743fd2f1d22241d8073
Zachariah Young <71157485+zachariahy@users.noreply.github.com>
2025-01-31 17:48:56 -0600

    Optimize median maintenance solution using two heaps. Implement solution using one binary search tree. Passed both test cases.

--- Assignments/Week 7/median_maintenance.py
+++ Assignments/Week 7/median_maintenance.py
@@ -1,55 +1,47 @@
-import heapq
-from audioop import reverse
-from cgitb import small
-from itertools import accumulate
-
-from bintrees import RBTree
-from Assignments import utilities
 from Assignments.utilities import read_numbers_from_file
-
+import heapq
+from bintrees import RBTree
 
 ROOT = 0
 
-def median_maintenance(nums: list):
+def median_maintenance_heaps(nums: list):
     accumulator = 0
-
     small_max_heap = []
     large_min_heap = []
 
     for k, num in enumerate(nums):
-        maximum = -small_max_heap[ROOT] if small_max_heap else None
-        minimum = large_min_heap[ROOT] if large_min_heap else None
-
-        len_small_max_heap = len(small_max_heap)
-        len_large_min_heap = len(large_min_heap)
-
-        if maximum is None or num < maximum:
+        if not small_max_heap or num < -small_max_heap[ROOT]:
             heapq.heappush(small_max_heap, -num)
-            if k % 2 == 1 and len_small_max_heap > len_large_min_heap:
-                item = -heapq.heappop(small_max_heap)
-                heapq.heappush(large_min_heap, item)
-        elif minimum is None or num > minimum:
+        else:
             heapq.heappush(large_min_heap, num)
-            if k % 2 == 1 and len_large_min_heap > len_small_max_heap:
-                item = heapq.heappop(large_min_heap)
-                heapq.heappush(small_max_heap, -item)
-        else:
-            if len_small_max_heap > len_large_min_heap:
-                heapq.heappush(large_min_heap, num)
-            else:
-                heapq.heappush(small_max_heap, -num)
 
-        if len(large_min_heap) > len(small_max_heap):
-            median = large_min_heap[ROOT]
-        else:
-            median = -small_max_heap[ROOT]
+        if len(small_max_heap) > len(large_min_heap) + 1:
+            heapq.heappush(large_min_heap, -heapq.heappop(small_max_heap))
+        elif len(small_max_heap) < len(large_min_heap):
+            heapq.heappush(small_max_heap, -heapq.heappop(large_min_heap))
+
+        median = -small_max_heap[ROOT]
         accumulator += median
 
     return accumulator
 
+def median_maintenance_search_tree(nums: list):
+    tree = RBTree()
+    accumulator = 0
+
+    for k, num in enumerate(nums):
+        tree.insert(num, num)
+
+        median = tree.nsmallest((k // 2) + 1)[-1][0]
+        accumulator += median
+
+    return accumulator
 
 if __name__ == "__main__":
     # data_set = read_numbers_from_file("Median_test.txt")    # 29335
-    data_set = read_numbers_from_file("Median_challenge.txt")
-    kth_median = median_maintenance(data_set)   # 46831213
+    data_set = read_numbers_from_file("Median_challenge.txt")   # 46831213
+
+    # kth_median = median_maintenance_heaps(data_set)
+    kth_median = median_maintenance_search_tree(data_set)   # much slower
+
     print(kth_median)
