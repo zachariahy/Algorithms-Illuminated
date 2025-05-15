afed35e0353b7850523b8e2d34e82eabbc7d554b
Zachariah Young <71157485+zachariahy@users.noreply.github.com>
2025-05-15 16:47:18 -0500

    Implement the Floyd-Warshall all-paths shortest path algorithm.

--- Assignments/W13: All-Pairs Shortest Path/apsp.py
+++ Assignments/W13: All-Pairs Shortest Path/apsp.py
@@ -54,10 +54,7 @@ def bellman_ford(graph: dict, s: int) -> list | None:
 
     return None
 
-def floyd_warshall():
-    pass
-
-if __name__ == "__main__":
+def all_min_paths_reduction():
     # g = read_directed_weighted_edge_list("problem18.8test1.txt") # -2
     # g = read_directed_weighted_edge_list("problem18.8test2.txt") # Negative Cycle
     # g = read_directed_weighted_edge_list("g1.txt")  # Negative Cycle
@@ -72,6 +69,45 @@ if __name__ == "__main__":
         if min_paths is None:
             print("Negative Cycle")
             quit()
+
+    min_min_path = min([path for sublist in all_min_paths for path in sublist])
+    print(min_min_path)
+
+def floyd_warshall(graph: dict) -> list | str:
+    n = len(graph)
+    dp = [[[math.inf for _ in range(n)] for _ in range(n)] for _ in range(n+1)]
+    # base cases
+    for v in range(n):
+        for w in range(n):
+            if v == w:
+                dp[0][v][w] = 0
+            # elif v in [tail for tail, cost in graph[w][IN_EDGES]]:
+            elif w in [head for head, cost in graph[v][OUT_EDGES]]:
+                dp[0][v][w] = [cost for head, cost in graph[v][OUT_EDGES] if head == w][0]
+            else:
+                dp[0][v][w] = math.inf
+    # solve subproblems
+    for k in range(1, n):
+        for v in range(n):
+            for w in range(n):
+                case_1 = dp[k-1][v][w]
+                case_2 = dp[k-1][v][k] + dp[k-1][k][w]
+                dp[k][v][w] = min(case_1, case_2)
+    # check for negative cycle
+    for v in range(n):
+        if dp[n-1][v][v] < 0:
+            return None
+    return dp[n-1]
+
+if __name__ == "__main__":
+    # all_min_paths_reduction()
+    g = read_directed_weighted_edge_list("problem18.8test1.txt")
+    # g = read_directed_weighted_edge_list("problem18.8test2.txt")
+    all_min_paths = floyd_warshall(g)
+
+    if all_min_paths is None:
+        print("Negative Cycle")
+        quit()
 
     min_min_path = min([path for sublist in all_min_paths for path in sublist])
     print(min_min_path)
